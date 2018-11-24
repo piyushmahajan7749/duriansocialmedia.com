d8aea1bdbb41cb2098c0e967cfdfbf0d86f8f4b2
piyushmahajan774 <piyushmahajan7749@gmail.com>
2018-11-24 01:43:18 -0500

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/pmahajan/Projects/SMMA/gatsby/durian-social/durian-social-media/duriansocialmedia.com/node_modules/gatsby-plugin-emotion/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/pmahajan/Projects/SMMA/gatsby/durian-social/durian-social-media/duriansocialmedia.com/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    }]
--- .cache/sync-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-
-exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": preferDefault(require("/Users/pmahajan/Projects/SMMA/gatsby/durian-social/durian-social-media/duriansocialmedia.com/node_modules/gatsby-plugin-offline/app-shell.js")),
-  "component---src-pages-index-jsx": preferDefault(require("/Users/pmahajan/Projects/SMMA/gatsby/durian-social/durian-social-media/duriansocialmedia.com/src/pages/index.jsx"))
-}
-
